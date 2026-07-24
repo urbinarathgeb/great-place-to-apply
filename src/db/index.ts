ee44c106d9b9fbc2f23beeb44dc8ca187385073b
Javier Urbina Rathgeb <urbinarathgeb@gmail.com>
2026-07-24 12:55:36 -0400

    fix: usar process.env.NODE_ENV para compatibilidad con tsx

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ import postgres from 'postgres';
 config({ path: '.env.development' });
 
 const client = postgres(process.env.DATABASE_URL!, {
-  max: import.meta.env.PROD ? 5 : 10,
+  max: process.env.NODE_ENV === 'production' ? 5 : 10,
 });
 
 export const db = drizzle({ client });
